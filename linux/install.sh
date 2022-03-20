452b1af616edb87e7c74e14e5107d89c939f7f7e
Hong-Shao <joee0511@gmail.com>
2022-03-20 23:16:48 +0800

    write full content of installation script

--- linux/install.sh
+++ linux/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+
+rm -fr $(brew --repo homebrew/core)
+brew tap homebrew/core
+
 sudo brew install docker
 sudo brew install docker-compose
 sudo brew install openjdk@11
@@ -8,3 +14,4 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 sudo brew install python@3.10
 sudo brew install node
 sudo brew install mysql
+
