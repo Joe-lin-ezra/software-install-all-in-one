ba611f1bb2884d541b46151f9a9126e60329ce59
Hong-Shao <joee0511@gmail.com>
2022-03-25 12:16:32 +0800

    delete "sudo" snipper

--- linux/install.sh
+++ linux/install.sh
@@ -6,12 +6,12 @@ export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 rm -fr $(brew --repo homebrew/core)
 brew tap homebrew/core
 
-sudo brew install docker
-sudo brew install docker-compose
-sudo brew install openjdk@11
+brew install docker
+brew install docker-compose
+brew install openjdk@11
 export JAVA_HOME=/path/to/openjdk
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-sudo brew install python@3.10
-sudo brew install node
-sudo brew install mysql
+brew install python@3.10
+brew install node
+brew install mysql
 
